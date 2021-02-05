277dee47cb8d4d165ca27470ea7ce42f74c75850
zjl0001 <zjl0001@mix.wvu.edu>
2021-02-05 14:18:05 -0500

    'master'

---
+++ index.php
@@ -0,0 +1,50 @@
+<main>
+    <link rel = "stylesheet" href="css/about.css">
+
+    <div class="about">
+      <h1>About Us</h1>
+      <p>klasjdfflkjsklfjdl;sfjksldfjl;sflf;alsfjsd;f</p>
+    </div>
+    <div class="row">
+      <div class="column">
+        <div class="card">
+          <img src="images/DarthVader.jpeg">
+          <div class="container">
+            <h2>CEO</h2>
+            <p>Darth Vader</p>
+            <p>Email:masterSkywalker@cs230.com</p>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <img src="images/Luke.jpeg">
+          <div class="container">
+            <h2>President</h2>
+            <p>Luke Skywalker</p>
+            <p>Email:skywalker@cs230.com</p>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <img src="images/Leia.jpeg">
+          <div class="container">
+            <h2>Vice President</h2>
+            <p>Leia Organa</p>
+            <p>Email:organa@cs230.com</p>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <img src="images/Chewy.jpeg">
+          <div class="container">
+            <h2>CFO</h2>
+            <p>Chewbacca</p>
+            <p>Email:chewy@cs230.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>
