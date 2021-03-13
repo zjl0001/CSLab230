f6e725e31202c714587c0e8528f5a785e927d0e3
zjl0001 <zjl0001@mix.wvu.edu>
2021-03-12 22:00:55 -0500

    lab6

--- includes/header.php
+++ includes/header.php
@@ -41,7 +41,10 @@ session_start();
             <a class = "nav-link" href = "gallery.php">Gallery</a>
             </li>
             <li class="nav-item">
-                <a class = "nav-link" href="profile.php>Profile</a>
+            <a class = "nav-link" href="profile.php">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" href="admin.php">Admin</a>
             </li>';
             }
             else{
