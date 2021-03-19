41b47e17f85e23e8f9a936e1004c19b466dd0a15
zjl0001 <zjl0001@mix.wvu.edu>
2021-03-19 18:35:48 -0400

    lab7

---
+++ display-reviews.php
@@ -0,0 +1,44 @@
+<?php
+
+$servename = "localhost";
+$DBuname = "phpmyadmin";
+$DBPass = "cs230lab";
+$DBname = "cs230";
+
+$conn = mysqli_connect($servename, $DBuname, $DBPass, $DBname);
+
+if (!$conn) {
+    die("Connection failed...".mysqli_connect_error());
+    # code...
+}
+
+$item = $_GET['id'];
+
+$sql="SELECT * FROM reviews WHERE itemid='$item'";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result)>0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        $uname = $row['uname'];
+        $propic = "SELECT profpic FROM profiles WHERE uname='$uname';";
+        $res = mysqli_query($conn, $propic);
+        $picpath = mysqli_fetch_assoc($res);
+
+        echo '<div class="card mx-auto" style="width: 30%; padding: 5px; margin-bottom: 10px;">
+        <div class="media">
+            <img class="mr-3" src="'.$picpath['profpic'].'" style="max-width: 75px; max-height: 75px; border-radius: 50%;">
+            <div class="media-body">
+              <h4 class="mt-0">'.$row['uname'].'</h4>
+              <h5 style="color: red">Rating: '.$row['ratingnum'].'</h5>
+              <p>'.$row['revdate'].'</p>
+              <p>'.$row['reviewtext'].'</p>
+            </div>
+          </div>
+    
+    </div>';
+    }
+}
+else {
+    echo 'h5 style="text-align: center;">No reveiws, yet! Be the First!</h5>';
+}
--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,4 @@
-<!-- <!DOCTYPE html>
-<html> -->
-<?php
+<?php 
 session_start();
 ?>
 
@@ -10,17 +8,13 @@ session_start();
     <title>Star Wars Cite</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/0809ee8fa6.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -33,54 +27,31 @@ session_start();
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <?php if (isset($_SESSION['uid'])){
-                echo '<li class = "nav-item">
-                <a class = "nav-link" href="includes/logout.php">Logout</a>
-            </li>
-            <li class="nav-item">
-            <a class = "nav-link" href = "gallery.php">Gallery</a>
-            </li>
-            <li class="nav-item">
-            <a class = "nav-link" href="profile.php">Profile</a>
-            </li>
-            <li class="nav-item">
-                <a class = "nav-link" href="admin.php">Admin</a>
-            </li>';
-            }
-            else{
-                echo '<li class = "nav-item">
-                <a class ="nav-link" href="login.php">Login</a>
-                </li>'; 
-            }
-            ?>
+                    <?php if (isset($_SESSION['uid'])) {
+                        echo '<li class="nav-item">
+                        <a class="nav-link" href="includes/logout.php">Logout</a>
+                    </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../index.php">Home</a>
+                        <a class="nav-link" href="gallery.php">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profile.php">Profile</a>
+                    </li>';
+                    }
+                    else {
+                        echo '<li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="signup.php">Signup</a>
+                    </li>';
+                    }
+                    ?>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 </header>
-
-<!-- <li class="nav-item">
-                    <a class="nav-link" href="login.html">Login</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Reviews
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Replace Me</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Replace Me 2</a>
-                    </div>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form> -->
