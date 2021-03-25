5bdd013e9d41d970e70c3bd09eb2f226fbd7147c
zjl0001 <78039181+zjl0001@users.noreply.github.com>
2021-03-25 13:18:02 -0400

    Update signup-helper.php

--- includes/signup-helper.php
+++ includes/signup-helper.php
@@ -42,7 +42,7 @@ if(isset($_POST['signup-submit'])){
                 }
                 else{
                     $hashed = password_hash($passw, PASSWORD_BCRYPT);
-                    mysqli_stmt_bind_param($stmt, "sssss",$lname, $fname, $email, $username, $hashedPass);
+                    mysqli_stmt_bind_param($stmt, "sssss",$lname, $fname, $email, $username, $hashed);
                     mysqli_stmt_execute($stmt);
                     mysqli_stmt_store_result($stmt);
 
